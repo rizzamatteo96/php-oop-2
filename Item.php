bc213801c66c99ac19718e0c480a14248b8f4f37
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-02 13:49:16 +0200

    init project

---
+++ Item.php
@@ -0,0 +1,14 @@
+<?php
+
+class Item{
+
+  protected $name;
+  protected $price;
+
+  public function __construct($_name, $_price){
+    $this->name = $_name;
+    $this->price = $_price;
+  }
+
+
+}
